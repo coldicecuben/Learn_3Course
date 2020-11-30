84a3afb790831e888bde09f6f285807ad59419ed
coldicecuben <ardwiz9@gmail.com>
2020-11-30 13:09:14 +0200

    first commit

---
+++ MO/Lab1/Lab1_NonQuad/cmake-build-debug/CMakeFiles/Lab1_NonQuad.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1_NonQuad.dir/main.cpp.o"
+  "Lab1_NonQuad"
+  "Lab1_NonQuad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab1_NonQuad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
